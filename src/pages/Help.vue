575f78cc95026243af830370d5d8240ab12ffd22
Tempestfury11 <swiftyblue11@gmail.com>
2022-09-30 09:18:15 +0200

    final push

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,8 +1,16 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Help</h5>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quibusdam totam facere minus. Dolorum architecto, sint alias nemo incidunt debitis eveniet quidem hic magni, perspiciatis earum mollitia assumenda in impedit!</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi quibusdam totam facere minus. Dolorum architecto, sint alias nemo incidunt debitis eveniet quidem hic magni, perspiciatis earum mollitia assumenda in impedit!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Sequi quibusdam totam facere minus.
+      Dolorum architecto, sint alias nemo incidunt debitis eveniet
+      quidem hic magni, perspiciatis earum mollitia assumenda in
+      impedit!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Sequi quibusdam totam facere minus.
+      Dolorum architecto, sint alias nemo incidunt debitis eveniet
+      quidem hic magni, perspiciatis earum mollitia assumenda in
+      impedit!</p>
   </q-page>
 </template>
 
